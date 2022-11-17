d5e14b847f33a6f6310e5198ac18b9e0b094a70b
Karunya A <karunya9898@gmail.com>
2022-11-17 13:36:42 +0530

    Commit all

---
+++ gitfile.java
@@ -0,0 +1,13 @@
+public class gitfile {
+	public static void main(String[] args)
+ {
+                int a=2;
+		int b=8;
+                System.out.println(a+b);
+		int mul=a*b;
+		System.out.println(mul);
+		System.out.println("Continuous Integration");
+}
+}
+// Committing addition
+// checking whether my jenkins find my new commit
---
+++ newgitfile.java
@@ -0,0 +1,7 @@
+public class newgitfile {
+	public static void main(String[] args)
+ {
+		System.out.println("Continuous Deployment");
+}
+}
+//Committing multiplication git project
