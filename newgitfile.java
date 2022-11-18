86606a6403e6b9654776cdb4fbba0e06c57b8caf
Karunya9898 <118271582+Karunya9898@users.noreply.github.com>
2022-11-18 12:25:36 +0530

    update

--- newgitfile.java
+++ newgitfile.java
@@ -2,6 +2,7 @@ public class newgitfile {
 	public static void main(String[] args)
  {              System.out.println("Continuous Integration");
 		System.out.println("Continuous Deployment");
+                System.out.println("Jenkins");
 }
 }
 //Committing multiplication git project
