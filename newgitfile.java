ec6d832ff1fa7f98970cdd535725f47bf03a600a
Karunya9898 <118271582+Karunya9898@users.noreply.github.com>
2022-11-18 12:22:50 +0530

    commit new change

--- newgitfile.java
+++ newgitfile.java
@@ -1,6 +1,6 @@
 public class newgitfile {
 	public static void main(String[] args)
- {
+ {              System.out.println("Continuous Integration");
 		System.out.println("Continuous Deployment");
 }
 }
